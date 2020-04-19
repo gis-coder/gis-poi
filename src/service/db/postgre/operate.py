22989bad492a94d41088fbc90c84555f4863b548
807702248@qq.com <807702248@qq.com>
2020-04-19 23:54:45 +0800

    修改配置文件信息读取路径问题

--- src/service/db/postgre/operate.py
+++ src/service/db/postgre/operate.py
@@ -7,7 +7,8 @@ import psycopg2
 
 
 def __post_conn():
-    cfg_path = os.path.join(os.path.abspath(os.path.join(os.getcwd(), "..")), 'service.cfg')
+    cfg_path = os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), 'service.cfg')
+
     postgre_cfg = configparser.ConfigParser()
     postgre_cfg.read(cfg_path)
 
@@ -23,25 +24,24 @@ def __post_conn():
 
 
 def run(sql):
+    if sql is None or str.strip(sql) == '':
+        raise Exception('查询语句为空')
+    run_conn = __post_conn()
     try:
-
-        if sql is None or str.strip(sql) == '':
-            raise Exception('查询语句为空')
-        db_conn = __post_conn()
-        db_cursor = db_conn.cursor()
+        db_cursor = run_conn.cursor()
         db_cursor.execute(sql)
     except Exception as e:
         print(e)
     finally:
-        db_conn.close()
+        run_conn.close()
 
 
 def __select(sql, type):
+    if sql is None or str.strip(sql) == '':
+        raise Exception('查询语句为空')
+    select_conn = __post_conn()
     try:
-        if sql is None or str.strip(sql) == '':
-            raise Exception('查询语句为空')
-        db_conn = __post_conn()
-        db_cursor = db_conn.cursor()
+        db_cursor = select_conn.cursor()
         db_cursor.execute(sql)
         if type == 0:
             row = db_cursor.fecthone()
@@ -58,7 +58,7 @@ def __select(sql, type):
     except Exception as e:
         print(e)
     finally:
-        db_conn.close()
+        select_conn.close()
 
 
 def post_select(sql):
@@ -77,7 +77,9 @@ def post_select_all(sql):
 
 if __name__ == '__main__':
     try:
-        sql = 'select * from poi'
-        __select(sql,1)
+        sql = "select st_astext(geom) as geom from tb_region where f_code = '510000';"
+        rows = __select(sql, 1)
+        for row in rows:
+            print(row)
     except Exception as e:
         print(e)
